6d76702d16f62bdea710b1bf64ac5f457539e3e1
laiamanda <40805270+laiamanda@users.noreply.github.com>
2026-04-05 13:18:37 -0400

    Add tasks.component.ts

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { NewTaskComponent } from './new-task/new-task.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { TasksServices } from './tasks.service';
+
+@Component({
+  selector: 'app-tasks',
+  standalone: true,
+  templateUrl: './tasks.component.html',
+  imports: [NewTaskComponent, TasksListComponent],
+  // providers: [TasksServices], // limited
+})
+export class TasksComponent {}
